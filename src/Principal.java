bfe1865672aae9d98e8b01d6d983fae9316ce41f
Valeria Pequeno <69013969+vpequeno@users.noreply.github.com>
2021-11-26 10:31:43 +0000

    Initial commit

---
+++ src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    /*
+    Coloque aqui o main, as instruções para criar as 10 músicas em diferentes formatos
+    e que depois faça  uma busca por músicas dos anos 90, imprimindo-a numa lista.
+    A impressão deve mostrar as informações armazenadas em todos os atributos dos objetos.
+     */
+}
